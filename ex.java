c0a005335d8383479e65907ae304fd4d60bfa9b9
MALKARAJ-FULLCREATIVE <malkaraj.karup@anywhere.co>
2021-03-03 12:31:08 +0530

    changed made frrom malkaraj-fullcreative at 03-03-2021

--- ex.java
+++ ex.java
@@ -6,13 +6,16 @@ class new{
 				System.out.println("George");
 
 				System.out.println("Malkaraj");
-<<<<<<< HEAD
+
                                  //again edited by George
    	}
-=======
                                  //again edited from malkaraj-fullcreative 
                                 System.out.println("malkara-fullcreative again checking");
 
-	}                     
->>>>>>> 6e6a53de45eeb758c4939ce9bdda3f96177a8997
+
+                                 //again edited from malkaraj-fullcreative
+				 System.out.printf("  ");
+
+	}                        
+
 }
