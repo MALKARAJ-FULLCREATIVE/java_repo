0fd6792da2a6fac2e457547614fe96c1eaff2c9b
FullCreative2021 <georgejoseph44435@gmail.com>
2021-03-03 11:41:34 +0530

    edited from malkaraj-fullcreative

--- ex.java
+++ ex.java
@@ -2,6 +2,10 @@ class new{
 
   public static void main(String args[])
    	{
-                                 System.out.println("George");
+                                //System.out.println("George");
+				System.out.println("George");
+
+				System.out.println("Malkaraj");
+
    	}
 }
